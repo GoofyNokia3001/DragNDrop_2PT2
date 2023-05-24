aae39ad4667677d559dda956bb06385b3ef6b91c
Klim <ixibiuz@gmail.com>
2023-05-24 12:19:23 +0300

    Script Start

---
+++ New Unity Project/Assets/Scripts/DragNDrop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragNDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+
+    public Object ObjectScript;
+    private CanvasGroup canvasGroup;
+    private RectTransform dragObjRectTransform;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+       
+    }
+
+    // Use this for initialization
+    void Start () {
+		canvasGroup= GetComponent<CanvasGroup>();
+        dragObjRectTransform= GetComponent<RectTransform>();
+	}
+	
+}
