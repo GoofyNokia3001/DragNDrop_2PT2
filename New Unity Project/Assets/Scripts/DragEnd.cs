37d7aa979ae5d2821c76691b36ee7ccbf22102b2
Klim <ixibiuz@gmail.com>
2023-05-25 15:53:15 +0300

    Script Start

---
+++ New Unity Project/Assets/Scripts/DragEnd.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragEnd : MonoBehaviour, IDropHandler{
+	private float placeZRotat, dragObjZRotat, rotationDiff;
+	private Vector2 placeSize, dragObjSize;
+	private float xSizeDiff, ySizeDiff;
+	public Objects ObjectScript;
+
+	
+
+    public void OnDrop(PointerEventData eventData)
+    {
+		if (eventData.pointerDrag != null) {
+			
+			if (eventData.pointerDrag.tag.Equals(tag)) {
+				placeZRotat = GetComponent<RectTransform>().transform.eulerAngles.z;
+				dragObjZRotat = eventData.pointerDrag.GetComponent<RectTransform>().transform.eulerAngles.z;
+				rotationDiff = Mathf.Abs(dragObjZRotat-placeZRotat);
+                dragObjSize = eventData.pointerDrag.GetComponent<RectTransform>().localScale;
+				placeSize = GetComponent<RectTransform>().localScale;
+				xSizeDiff = Mathf.Abs(dragObjSize.x - placeSize.x);
+                ySizeDiff = Mathf.Abs(dragObjSize.y - placeSize.y);
+
+				if ((rotationDiff <= 6 || (rotationDiff >= 354 && rotationDiff <= 360))
+					&& (xSizeDiff <= 0.1 && ySizeDiff <= 0.1)) 
+				{
+					ObjectScript.RightPlace = true;
+
+					eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+
+					eventData.pointerDrag.GetComponent<RectTransform>().localRotation = GetComponent<RectTransform>().localRotation;
+					
+					eventData.pointerDrag.GetComponent<RectTransform>().localScale= GetComponent<RectTransform>().localScale;
+
+					switch (eventData.pointerDrag.tag) 
+					{
+						case "garbage":
+							ObjectScript.audioSource.PlayOneShot(ObjectScript.AudioToPlay[0]);
+							break;
+
+                        case "ambulance":
+                            ObjectScript.audioSource.PlayOneShot(ObjectScript.AudioToPlay[1]);
+                            break;
+
+                        case "bus":
+                            ObjectScript.audioSource.PlayOneShot(ObjectScript.AudioToPlay[2]);
+                            break;
+
+                    }
+				}
+            } else {
+				ObjectScript.RightPlace = false;
+				ObjectScript.audioSource.PlayOneShot(ObjectScript.AudioToPlay[3]);
+                switch (eventData.pointerDrag.tag)
+                {
+                    case "garbage":
+						ObjectScript.GarbageCar.GetComponent<RectTransform>().localPosition = ObjectScript.GarbCCOORD;
+                        break;
+
+                    case "ambulance":
+                        ObjectScript.AmbulanceCar.GetComponent<RectTransform>().localPosition = ObjectScript.AmbulCCOORD;
+                        break;
+
+                    case "bus":
+                        ObjectScript.BusCar.GetComponent<RectTransform>().localPosition = ObjectScript.BusCCOORD;
+                        break;
+
+                }
+            }
+		}
+    }
+
+   
+}
---
+++ New Unity Project/Assets/Scripts/Objects.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour {
+
+    public GameObject GarbageCar;
+    public GameObject AmbulanceCar;
+    public GameObject BusCar;
+
+	[HideInInspector]
+	public Vector2 GarbCCOORD;
+
+	[HideInInspector]
+	public Vector2 AmbulCCOORD;
+
+    [HideInInspector]
+    public Vector2 BusCCOORD;
+
+	public Canvas canva;
+
+	public AudioSource audioSource;
+	public AudioClip[] AudioToPlay;
+
+	[HideInInspector]
+	public bool RightPlace = false;
+	public GameObject LastDrag = null;
+
+    // Use this for initialization
+    void Start () {
+		GarbCCOORD = GarbageCar.GetComponent<RectTransform>().localPosition;
+        AmbulCCOORD = AmbulanceCar.GetComponent<RectTransform>().localPosition;
+        BusCCOORD = BusCar.GetComponent<RectTransform>().localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
